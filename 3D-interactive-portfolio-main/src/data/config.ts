3be0bbfad448a8c6609626e802e6687e81af4c42
Abdul Raheman <abraheman.744@gmail.com>
2025-07-03 06:56:04 -0700

    first commit

---
+++ 3D-interactive-portfolio-main/src/data/config.ts
@@ -0,0 +1,36 @@
+const config = {
+  title: "Abdul Raheman | MERN Stack Developer",
+  description: {
+    long: "Explore the portfolio of Abdul Raheman, a Computer Science Engineer with 1.5 year experience in Development, DevOps & Database management with hands-on experience in deploying & automating workflows, & collaborating with foreign clients. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
+    short:
+      "Discover the portfolio of Abdul Raheman, a DevOps enthusiast solving real life problems in DevOps and Development.",
+  },
+   keywords: [
+    "Abdul",
+    "Abdul Raheman",
+    "portfolio",
+    "DevOps",
+    "Development",
+    "Database",
+    "web development",
+    "interactive websites",
+    "AWS",
+    "DevSecOps",
+    "Azure",
+  ],
+  author: "Abdul Raheman",
+  email: "abraheman.744@gmail.com",
+  // site: "https://abhijitzende.com",
+
+  // get ogImg() {
+  //   return this.site + "/assets/seo/og-image.png";
+  // },
+  social: {
+    twitter: "https://",
+    linkedin: "https://linkedin.com/in/abdul-raheman-3285502b8",
+    instagram: "https://www.instagram.com/dev_vibes_world",
+    // facebook: "https://www.facebook.com/profile.php?id=100010029821759",
+    github: "https://github.com/AbdulRaheman74",
+  },
+};
+export { config };
