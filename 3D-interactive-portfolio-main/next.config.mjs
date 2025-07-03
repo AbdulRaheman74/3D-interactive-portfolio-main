af2a4b935e2df20c40b32cc728745dfd5c1f0b33
Abdul Raheman <abraheman.744@gmail.com>
2025-07-03 12:49:32 -0700

    changes

--- 3D-interactive-portfolio-main/next.config.mjs
+++ 3D-interactive-portfolio-main/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true, // Enable app directory
+  },
+};
 
 export default nextConfig;
---
+++ 3D-interactive-portfolio-main/src/app/api/send/config .js
@@ -0,0 +1,3 @@
+// export const config = {
+//   email: "abraheman.744@gmail.com", // ✅ email jahan message bhejna hai
+// };
